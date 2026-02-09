502428644ae4b98207f581b70575b79a65620684
Jack J <115509970+jack3j@users.noreply.github.com>
2026-02-09 15:53:41 -0800

    Fix formatting again holy how many times do i need to do this

--- lib/libkoala.dart
+++ lib/libkoala.dart
@@ -4,7 +4,8 @@ export 'providers/auth_provider.dart'
     show authProvider, authStatusProvider, AuthStatus, Auth;
 export 'providers/device_info_provider.dart'
     show deviceInfoProvider, DeviceInfo, DeviceOS;
-export 'providers/api_provider.dart' show getDataProvider, getListDataProvider, honeycombClientProvider;
+export 'providers/api_provider.dart'
+    show getDataProvider, getListDataProvider, honeycombClientProvider;
 export 'providers/secure_storage_provider.dart' show secureStorageProvider;
 export 'providers/user_profile_provider.dart'
     show userInfoProvider, UserInfo, userProfileServiceProvider;
